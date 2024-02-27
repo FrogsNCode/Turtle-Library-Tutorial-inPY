57797a33b334a9dc4997a713c39f25178d23f650
FrogsNCode <161363521+FrogsNCode@users.noreply.github.com>
2024-02-27 16:34:32 +0100

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import turtle
 
+##################################
+# Initial Setup and Basic Movement
+##################################
+
 # Initial setup
 screen = turtle.Screen()
 screen.setup(width=800, height=600)
@@ -8,7 +12,33 @@ screen.bgcolor("white")
 
 # Create turtle
 t = turtle.Turtle()
-t.speed(20)  # Set turtle speed
+t.speed(1)  # Set turtle speed
+
+# Basic movement
+t.forward(100)  # Move forward by 100 pixels
+t.left(90)      # Turn left by 90 degrees
+t.forward(100)
+
+#########################
+# Drawing Simple Shapes
+#########################
+
+# Draw a square
+for _ in range(4):
+    t.forward(100)
+    t.right(90)
+
+# Draw a circle
+t.circle(50)
+
+# Draw a triangle
+for _ in range(3):
+    t.forward(100)
+    t.left(120)
+
+#################################################
+# Using Loops and Functions for Complex Drawings
+#################################################
 
 # Draw a hexagon
 def draw_hexagon():
@@ -17,6 +47,7 @@ def draw_hexagon():
         t.left(60)
 
 draw_hexagon()
+
 
 # Draw a spiral
 def draw_spiral():
